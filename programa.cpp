27727623b10b7c4c81c5bbab1906fa51b4a51ef4
juliusberzinskas <145833981+juliusberzinskas@users.noreply.github.com>
2023-11-05 11:20:51 +0100

    Update programa.cpp

--- programa.cpp
+++ programa.cpp
@@ -1,4 +1,5 @@
-﻿#include "biblioteka.h"
+#include "biblioteka.h"
+
 
 void readDataFromFile(vector<Student>& Group, const string& filename) {
 	std::ifstream file(filename);
@@ -21,17 +22,70 @@ void readDataFromFile(vector<Student>& Group, const string& filename) {
 	file.close();
 }
 
-int main()
-{
-	vector<Student> Group;
-	string filename = "studentai10000.txt";
-	readDataFromFile(Group, filename); // Skaitome failą TIK KARTĄ
-
-	for (int i = 0; i < 3; i++) {
-		for (auto& duom : Group) {
-			cout << duom; // Atspausdiname studentų duomenis 3 kartus
+void categorizeStudents(const vector<Student>& students, vector<Student>& vargsiukai, vector<Student>& kietiakiai) {
+	for (const auto& student : students) {
+		if (student.getResult() < 5.0) {
+			vargsiukai.push_back(student);
+		}
+		else {
+			kietiakiai.push_back(student);
 		}
 	}
+}
+
+int main()
+{
+
+	vector<Student> Group;
+	vector<Student> Vargsiukai;
+	vector<Student> Kietiakai;
+	string filename = "studentai10000.txt";
+	readDataFromFile(Group, filename);
+	categorizeStudents(Group, Vargsiukai, Kietiakai);
+
+
+	cout << "Vargsiukai:" << endl;
+	cout << endl;
+	for (const auto& student : Vargsiukai) {
+		cout << student;
+	}
+
+	cout << "\nKietiakai:" << endl;
+	for (const auto& student : Kietiakai) {
+		cout << student;
+	}
+	// outputas atskiria studentus i vargsiukus ir kietiakus vargsiukai yra ispausdinami pirmi sarase o veliau eina kietiakai
+
+	// irasome vargsiukus i faila
+	std::ofstream vargsiukaiFile("vargsiukai.txt");
+	if (!vargsiukaiFile.is_open()) {
+		cout << "Nepavyko atidaryti vargsiukai.txt failo rašymui!" << endl;
+		return 1;
+	}
+
+	for (const auto& student : Vargsiukai) {
+		vargsiukaiFile << student;
+	}
+	vargsiukaiFile.close(); // Uzdarome faila
+
+	// irasome kietekus i faila
+	std::ofstream kietiakaiFile("kietiakai.txt");
+	if (!kietiakaiFile.is_open()) {
+		cout << "Nepavyko atidaryti kietiakai.txt failo rašymui!" << endl;
+		return 1;
+	}
+
+	for (const auto& student : Kietiakai) {
+		kietiakaiFile << student;
+	}
+	kietiakaiFile.close(); // Uzdarome faila
+
+	cout << "Vargsiukai ir kietiakai išsaugoti atitinkamuose failuose." << endl;
+
+	// programa sukuria failus direktorijoje vargsiukai.txt bei kietekai.txt
+
+
+	return 0;
 
 	system("pause");
 }
